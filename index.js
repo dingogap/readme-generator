e560f7e43b4ed101a9f0a4bbef4ac87334e2efad
Peter Medbury <peter.medbury@dingogap.net.au>
2023-07-20 13:10:07 +1000

    added error handling to functom that creates README.md file

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+// Create file filename & write data to it
+// Log errors to console
+// Log success to console 
+const writeToFile = (fileName, data) => {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`${fileName} created successfully`);
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() { }
 
 // Function call to initialize app
 init();
+const fileName = './output/README.md'
+writeToFile(fileName, '# README Generator'+"\n")
